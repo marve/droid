e118a93c9312c14d84d00231c2e5c94fcba55836
Daniel Charles <pdanielcharles@gmail.com>
2020-11-24 22:04:08 -0800

    Fix issue that stopped mounts from being cleaned up.

--- uninstall.sh
+++ uninstall.sh
@@ -7,7 +7,7 @@ echo "Looking for services linked to $scriptDir"
 for path in $(ls -lA /etc/systemd/system | grep $scriptDir | rev | cut -d' ' -f 1 | rev); do
     serviceFile=$(basename "$path")
     echo "Uninstalling a service: $serviceFile at $path"
-    sudo systemctl stop $serviceFile
+    sudo systemctl stop $serviceFile || :
     sudo systemctl disable $serviceFile
     sudo systemctl daemon-reload
     sudo systemctl reset-failed
